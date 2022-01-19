d36968884946cb0256fda0e3db91c64ae77186b7
Thato Ramalepa <tramalepa@my.wctc.edu>
2022-01-18 22:21:40 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// Repository:  country
+// Author:      Thato Ramalepa
+// Version:     1.xx
+import React, { Component } from 'react';
+import './App.css';
+
+import Country from './components/Country';
+
+class App extends Component {
+  render() { 
+    return ( 
+      <div className="App">
+        <header className="App-header">
+          Medal Count
+        </header>
+        <Country />
+      </div>
+     );
+  }
+}
+ 
+export default App;
---
+++ src/components/Country.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+
+
+class Country extends Component {
+    // state = {
+    //     country: 'USA',
+    //     count: 'Gold Medal'
+    // }
+    constructor (props) {
+        super(props);
+        this.state = {
+            country: 'USA',
+            count: 0
+        }
+    }
+
+    increament = () => {
+        this.setState({
+            count: this.state.count + 1
+        });
+    }
+
+    render() {
+        return (
+            <>
+                <div>
+                    {this.state.country}
+                    
+                </div>
+
+                <div>
+                    Gold Medals : {this.state.count}  
+                    <AddBoxIcon
+                        onClick={this.increament}
+                        
+                    />
+                    
+                    {/* option2 */}
+                    {/* <button 
+                        onClick={this.increament}
+                        > +
+                    </button> */}
+                    
+                </div>
+
+                
+            </>
+        );
+    }
+}
+
+export default Country;
